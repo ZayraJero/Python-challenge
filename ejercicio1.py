76a4058c51ddfc049a93ec4fc89c783956beb84c
Zayra Jeronimo <zayrajero@gmail.com>
2021-02-20 13:04:41 -0600

    exercise correction

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,11 +1,13 @@
 def number_lenght(number_param):
-      if number_param > 0: #Validamos que no sea negativo
+    if not isinstance(number_param, int): #Validamos que no sea negativo pero si entero
+        return None, "Tiene que ser un núnmero"
+    elif number_param < 0: 
+        return None, "No usar números negativos" #Para ser doble comprobación
         num_str = str(number_param) 
         count = 0 #Contador que se va incrementando en cada iteración
-        for element in num_str:
-            count += 1; #Se va iterando 1 por 1 
-        return count
-      else:
-        return None, "No usar números negativos" #Para ser doble comprobación
+    for element in num_str:
+        count += 1; #Se va iterando 1 por 1 
+    return count
 
 number_lenght(459687)
+number_lenght(True)
